99494ab3dd8ff9636793b72a85fc1224d8ec90ae
Luiznt <luiznt@hotmail.com>
2021-05-14 12:31:08 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,25 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		
+		List<Category> list = repository.findAll();
+		
+		/*
+		// simple way to load into listDto  
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for (Category cat: list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		return listDto;
+		*/
+
+		//another way to load listDto, more "elegant"
+		// List<CategoryDTO> listDto = list.stream().map(x-> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		// but it can return the listDto directly. Great!
+		return list.stream().map(x-> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 
 }
